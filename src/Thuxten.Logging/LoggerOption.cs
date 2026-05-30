df87e8cf369f0d74c78fbb3e4a5fbb30c2bd6425
thushxr <thushar8p@gmail.com>
2026-05-30 17:34:51 +0530

    prcs[2]: added masking options

--- src/Thuxten.Logging/LoggerOption.cs
+++ src/Thuxten.Logging/LoggerOption.cs
@@ -6,6 +6,13 @@ public class LoggerOption
 {
     internal bool StructuredLogging { get; private set; } = false;
     internal LogLevel MinimumLogLevel { get; private set; } = LogLevel.Information;
+    internal List<MaskingRule> MaskingRules { get; } = new();  
+
+    public LoggerOption MaskData(params string[] fields) 
+    {
+        MaskingRules.AddRange(fields.Select(f => new MaskingRule(f)));
+        return this;
+    }
     
     public LoggerOption UseStructuredLogging()
     {
